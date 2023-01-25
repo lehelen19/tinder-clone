a9295c760c0a26e62b5ff00293b130c783433719
Helen <le.helen19@gmail.com>
2023-01-24 17:37:20 -0800

    Initialize Tinder cards from library.
    Install react-tinder-card library and use example code to create tinder
    cards with swipe left/right functionality.

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,5 +1,58 @@
+import { useState } from 'react';
+import TinderCard from 'react-tinder-card';
+
+const db = [
+  {
+    name: 'Richard Hendricks',
+    url: 'https://i.imgur.com/oPj4A8u.jpeg',
+  },
+  {
+    name: 'Jared Dunn',
+    url: 'https://i.imgur.com/oPj4A8u.jpeg',
+  },
+  {
+    name: 'Dinesh Chugtai',
+    url: 'https://i.imgur.com/oPj4A8u.jpeg',
+  },
+];
+
 const Dashboard = () => {
-  return <h1>dASHBOARD</h1>;
+  const characters = db;
+  const [lastDirection, setLastDirection] = useState();
+
+  const swiped = (direction, nameToDelete) => {
+    console.log('removing: ' + nameToDelete);
+    setLastDirection(direction);
+  };
+
+  const outOfFrame = (name) => {
+    console.log(name + ' left the screen!');
+  };
+
+  return (
+    <div className="dashboard">
+      {/* <ChatContainer /> */}
+      <div className="swiper-container">
+        <div className="card-container">
+          {characters.map((character) => (
+            <TinderCard
+              className="swipe"
+              key={character.name}
+              onSwipe={(dir) => swiped(dir, character.name)}
+              onCardLeftScreen={() => outOfFrame(character.name)}
+            >
+              <div
+                style={{ backgroundImage: 'url(' + character.url + ')' }}
+                className="card"
+              >
+                <h3>{character.name}</h3>
+              </div>
+            </TinderCard>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
