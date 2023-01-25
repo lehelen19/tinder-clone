bd874b42846570a7515693ad55b200f8d386e419
Helen <le.helen19@gmail.com>
2023-01-24 17:59:59 -0800

    Set up skeleton for ChatHeader

--- src/components/ChatHeader.js
+++ src/components/ChatHeader.js
@@ -1,5 +1,15 @@
 const ChatHeader = () => {
-  return <div>ChatHeader</div>;
+  return (
+    <div className="chat-container-header">
+      <div className="profile">
+        <div className="image-container">
+          <img src="" alt="yes" />
+        </div>
+        <h3>Username</h3>
+      </div>
+      <i className="log-out-icon">=</i>
+    </div>
+  );
 };
 
 export default ChatHeader;
