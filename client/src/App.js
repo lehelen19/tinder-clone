e49c371e05b59182096a66dd8bd776b1720ac176
Helen <le.helen19@gmail.com>
2023-01-30 18:58:27 -0800

    Update app routing.
    Show dashboard and onboarding pages only if user is logged in (if
    authToken is present).

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useCookies } from 'react-cookie';
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
 import Onboarding from './pages/Onboarding';
 
 const App = () => {
+  const [cookies, setCookie, removeCookie] = useCookies(['user']);
+  const authToken = cookies.AuthToken;
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/onboarding" element={<Onboarding />} />
+        {authToken && <Route path="/dashboard" element={<Dashboard />} />}
+        {authToken && <Route path="/onboarding" element={<Onboarding />} />}
       </Routes>
     </BrowserRouter>
   );
